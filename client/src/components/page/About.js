cebfadcf01f18487d9f97a96d5742f8a09e011ae
Lionel <lion6396643@hotmail.com>
2017-09-14 00:46:46 +0800

    Change about page

--- client/src/components/page/About.js
+++ client/src/components/page/About.js
@@ -5,15 +5,16 @@ class About extends React.Component {
     render() {
         return (
             <div className="introduction">
-                <h2>About me</h2>
+                <h2>About me</h2>   
                 <h3>Skill</h3>
                 <ul>
-                    <li>Front-end: HTML(ejs)、CSS、Javascript(ES5、ES6)</li>
-                    <li>Back-end: node.js</li>
-                    <li>DB: MongoDB</li>
-                    <li>Version Control: Git</li>
-                    <li>Cloud Application Platform: AWS EC2</li>
-                    <li>nginx</li>
+                    <li>Programming Language: HTML、CSS、Javascript(ES5/ES6)、node.js</li>
+                    <li>Frontend Framework: React.js</li>
+                    <li>Backend Framwork: Express.js</li>
+                    <li>Database: MongoDB</li>
+                    <li>Version Control: Git/GitHub、SVN</li>
+                    <li>Exprence in AWS(EC2, S3, Lambda)</li>
+                    <li>Experience with unit test(Mocha)  </li>
                 </ul>
                 <hr />
                 <h3>學歷</h3>
@@ -51,11 +52,10 @@ class About extends React.Component {
                 <hr />
                 <h3>我的網站</h3>
                 <p>GitHub: <a href="https://github.com/Lionel223">https://github.com/Lionel223</a></p>
-                <p>Coding Notes(Technical): <a href="https://google1565.wordpress.com/">https://google1565.wordpress.com/</a></p>
                 <hr />
                 <h3>興趣</h3>
                 <ul>
-                    <li>程式技術學習、實作，目前關注於javascript與node.js</li>
+                    <li>程式技術學習、實作，目前專注在加深javascript、node.js的了解，並期望之後能學習其他語言</li>
                     <li>攝影</li>
                     <li>閱讀</li>
                 </ul>
